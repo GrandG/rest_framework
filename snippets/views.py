fcb0c3a0270d2a743b23f4a5b201fca10bd77150
Wayne <616311401@qq.com>
2018-03-04 22:39:19 +0800

    使用api_view

--- snippets/views.py
+++ snippets/views.py
@@ -5,42 +5,45 @@ from .models import Snippet
 from .serializers import SnippetSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def snippet_list(request):
     if request.method == 'GET':
         snippet_list = Snippet.objects.all()
         serializer = SnippetSerializer(snippet_list, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'POST':
-        data = JSONParser().parse(request)
+        data = request.data
         serializer = SnippetSerializer(data=data)
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def snippet_detail(request, id):
     try:
         snippet = Snippet.objects.get(id=id)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
-        data = JSONParser().parse(request)
+        data = request.data
         serializer = SnippetSerializer(snippet, data=data)
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
 
     if request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
