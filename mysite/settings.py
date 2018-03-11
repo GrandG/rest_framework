1b3cb5f659c3ee5695e1d801689c00c1b0b049fb
Wayne <616311401@qq.com>
2018-03-11 23:39:00 +0800

    部署的分支, 把DEBUG设为False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0)jzv9w87cqpp7ydz2hjy+n1lp0rf57e!n+v%0%5y7(fh$i3wm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']
 
